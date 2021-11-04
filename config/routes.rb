22bae383a7aecf46738fedc168e5312eda863140
Yogaisan Ramasawmy <yogaisan@gmail.com>
2021-11-04 17:57:52 +0400

    removed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'lists#index'
 
   resources :lists, only: %i[create index new show destroy] do
     resources :bookmarks, only: %i[new create destroy]
