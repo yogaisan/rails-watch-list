cab352033e566b4965ec2e449d6b823fe7cb6c27
Yogaisan Ramasawmy <yogaisan@gmail.com>
2021-11-04 17:35:05 +0400

    modified root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  get 'lists/new', to: 'lists/new'
 
   resources :lists, only: [:create, :index, :new, :show, :destroy] do
     resources :bookmarks, only: [ :new, :create, :destroy ]
